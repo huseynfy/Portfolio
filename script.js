b45599a9153996c2925177c65045255a753f047e
Huseyn Yusifov <huseyn1996@gmail.com>
2025-09-20 15:48:04 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".burgermenu").click(function () {
+  $("#labelformenuicon").click(function () {
     $(".burgeritems").slideToggle()
   })
   $("a").on('click', function (event) {
@@ -14,23 +14,49 @@ $(document).ready(function () {
     }
   });
 })
-var modal = document.getElementById("myModal");
 
-var btn = document.querySelector("#work");
+///MODALS
 
-var span = document.getElementsByClassName("close")[0];
+let allWorks = document.querySelectorAll('.work');
+let modals = document.querySelectorAll('.modal');
+let closes = document.querySelectorAll('.close');
 
-btn.onclick = function () {
-  modal.style.display = "block";
+
+for (let i = 0; i < allWorks.length; i++) {
+    allWorks[i].addEventListener('click',()=>{
+      modals[i].style.display = 'block'
+    })
+    closes[i].addEventListener('click',()=>{
+      modals[i].style.display = "none";
+    })
 }
-span.onclick = function () {
-  modal.style.display = "none";
+
+document.addEventListener('keydown',function(e){
+  if(e.key == 'Escape'){
+    for (let i = 0; i < modals.length; i++) {
+      modals[i].style.display = "none";
+    }
+  }
+})
+
+window.ontouchstart = function(event){
+  for (let i = 0; i < allWorks.length; i++) {
+    if(event.target == modals[i]){
+      modals[i].style.display = "none";
+    }
 }
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+}
+
+window.onclick = function(event){
+  for (let i = 0; i < allWorks.length; i++) {
+    if(event.target == modals[i]){
+      modals[i].style.display = "none";
+    }
   }
 }
+
+
+///BACK TO TOP
 const backtotop = document.getElementById('backtotop')
 
 window.onscroll = function () { scrollfunction() }
@@ -51,3 +77,42 @@ function scrollToTop() {
     scrollAnimation = setTimeout("scrollToTop()", 5);
   } else clearTimeout(scrollAnimation);
 }
+
+
+
+// Scrolling Animation
+
+const works = gsap.utils.toArray('.work');
+
+works.forEach(work=>{
+  const anim = gsap.fromTo(
+    work,
+    {
+      autoAlpha:0,
+      y:100,
+      x:-100,
+      rotate:-10
+    },
+    {
+      duration:0.6,
+      autoAlpha:1,
+      y:0,
+      x:0,
+      rotate:0
+    }
+  );
+  ScrollTrigger.create({
+    trigger:work,
+    animation:anim
+  })
+})
+
+function updateDate(){
+  let date = new Date();
+  let year = date.getFullYear();
+  document.getElementById('copyright').innerHTML = '© ' + year + ' Huseyn Yusifov. All rights reserved.';
+}
+
+updateDate();
+
+console.log('%cIF YOU TRY TO COPY THE WEBSITE, PLEASE EMAIL ME','font-size:24px;color:salmon;')
